06089ca6d972b42174f8a1dcaa508e9f7d1e4d81
Ihsan Arif Rahman <ihsan.android@gmail.com>
2018-02-13 10:04:59 +0700

    update manifest

--- box-library-register/src/main/java/org/box/fuse/Registration.java
+++ box-library-register/src/main/java/org/box/fuse/Registration.java
@@ -1,7 +1,9 @@
 package org.box.fuse;
 
+import javax.jws.*;
 import org.box.fuse.model.Member;
 
+@WebService(targetNamespace="http://localhost:40000/box/library/register") 
 public interface Registration {
 	String register(Member member);
 
