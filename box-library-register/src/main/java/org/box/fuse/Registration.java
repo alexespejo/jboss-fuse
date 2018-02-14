66bbe33abb96d0af296f882fda0e58ad06ca12d1
Ihsan Arif Rahman <ihsan.android@gmail.com>
2018-02-14 09:27:55 +0700

    practice creating Red Hat JBoss Fuse OSGi Project

--- box-library-register/src/main/java/org/box/fuse/Registration.java
+++ box-library-register/src/main/java/org/box/fuse/Registration.java
@@ -1,11 +1,15 @@
 package org.box.fuse;
 
-import javax.jws.*;
+//import javax.jws.*;
+
+import org.box.fuse.model.Book;
 import org.box.fuse.model.Member;
 
-@WebService(targetNamespace="http://localhost:40000/box/library/register") 
+//@WebService(targetNamespace="http://localhost:40000/box/library/register") 
 public interface Registration {
 	String register(Member member);
 
 	String unregister(String username);
+	
+	String addbook(Book book);
 }
