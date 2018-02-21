e26fecd3fcfd1eb9dfecc8c67311b9fa20262f33
Ihsan Arif Rahman <ihsan.android@gmail.com>
2018-02-21 17:22:06 +0700

    update deploy fuse project

--- deploy-fuse/src/main/java/com/redhat/training/jb421/WrapProcessor.java
+++ deploy-fuse/src/main/java/com/redhat/training/jb421/WrapProcessor.java
@@ -11,19 +11,19 @@ import org.slf4j.LoggerFactory;
 
 public class WrapProcessor implements Processor {
 
-    final private static Logger log = LoggerFactory.getLogger(WrapProcessor.class);
+	final private static Logger log = LoggerFactory.getLogger(WrapProcessor.class);
 
-    final private static DateFormat df = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss Z");
+	final private static DateFormat df = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss Z");
 
-    @Override
-    public void process(Exchange exchange) throws Exception {
-        log.info("Wrapping order...");
-        String orderXml = exchange.getIn().getBody(String.class);
-        exchange.getIn()
-                .setBody("<journal>\n" + "<timestamp>" + df.format(new Date()) + "</timestamp>\n" + "<fileName>"
-                        + exchange.getIn().getHeader("CamelFileName", String.class) + "</fileName>\n" + orderXml
-                        + "</journal>\n");
+	@Override
+	public void process(Exchange exchange) throws Exception {
+		log.info("Wrapping order...");
+		String orderXml = exchange.getIn().getBody(String.class);
+		exchange.getIn()
+				.setBody("<journal>\n" + "<timestamp>" + df.format(new Date()) + "</timestamp>\n" + "<fileName>"
+						+ exchange.getIn().getHeader("CamelFileName", String.class) + "</fileName>\n" + orderXml
+						+ "</journal>\n");
 
-    }
+	}
 
 }
