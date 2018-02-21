82d7247031b994084cc2de3dbe971adac19ff226
Ihsan Arif Rahman <ihsan.android@gmail.com>
2018-02-21 14:40:54 +0700

    update hybrid routes

--- hybrid-routes/src/main/java/com/redhat/training/jb421/FTPRouteBuilder.java
+++ hybrid-routes/src/main/java/com/redhat/training/jb421/FTPRouteBuilder.java
@@ -4,42 +4,23 @@ import org.apache.camel.builder.RouteBuilder;
 
 public class FTPRouteBuilder extends RouteBuilder {
 	private static String FTP_SERVER = "ftp://{{server.hostname}}:{{server.port}}";
-	private static String FTP_OPTIONS = "?username={{server.username}}&password={{server.password}}";
+	private static String FTP_OPTIONS = "?username={{server.username}}&password={{server.password}}&noop=true";
 	private static String DIRECT = "direct:header";
 	// private static String DIRECT = "direct:wrap";
-	
+
 	public static String SRC_FOLDER1 = "/Documents/jboss/test_order/order2/abc";
 	public static String SRC_FOLDER2 = "/Documents/jboss/test_order/order2/orly";
 	public static String SRC_FOLDER3 = "/Documents/jboss/test_order/order2/namming";
 
 	@Override
 	public void configure() throws Exception {
-		
-		from(FTP_SERVER + SRC_FOLDER1 + FTP_OPTIONS).to(DIRECT).log("Alamat server : "+FTP_SERVER+SRC_FOLDER1+FTP_OPTIONS);
-		from(FTP_SERVER + SRC_FOLDER2 + FTP_OPTIONS).to(DIRECT).log("Alamat server : "+FTP_SERVER+SRC_FOLDER2+FTP_OPTIONS);
-		from(FTP_SERVER + SRC_FOLDER3 + FTP_OPTIONS).to(DIRECT).log("Alamat server : "+FTP_SERVER+SRC_FOLDER3+FTP_OPTIONS);
+
+		from(FTP_SERVER + SRC_FOLDER1 + FTP_OPTIONS).to(DIRECT)
+				.log("Alamat server : " + FTP_SERVER + SRC_FOLDER1 + FTP_OPTIONS);
+		from(FTP_SERVER + SRC_FOLDER2 + FTP_OPTIONS).to(DIRECT)
+				.log("Alamat server : " + FTP_SERVER + SRC_FOLDER2 + FTP_OPTIONS);
+		from(FTP_SERVER + SRC_FOLDER3 + FTP_OPTIONS).to(DIRECT)
+				.log("Alamat server : " + FTP_SERVER + SRC_FOLDER3 + FTP_OPTIONS);
 	}
 
 }
-
-
-//public class FTPRouteBuilder extends RouteBuilder {
-//
-//	private static String FTP_SERVER = "ftp://{{server.hostname}}:{{server.port}}";
-//	private static String FTP_OPTION = "?username={{server.username}}&password={{server.password}}&noop=true";
-//	private static String DIRECT = "direct:header";
-////	private static String DIRECT = "direct:wrap";
-//
-//	public static String SRC_FOLDER1 = "/tempo";
-//	public static String SRC_FOLDER2 = "/gualgaol";
-//	public static String SRC_FOLDER3 = "/suppori";
-//
-//	@Override
-//	public void configure() throws Exception {
-//
-//		from(FTP_SERVER + SRC_FOLDER1 + FTP_OPTION).to(DIRECT);
-//		from(FTP_SERVER + SRC_FOLDER2 + FTP_OPTION).to(DIRECT);
-//		from(FTP_SERVER + SRC_FOLDER3 + FTP_OPTION).to(DIRECT);
-//	}
-//
-//}
--- hybrid-routes/src/main/java/com/redhat/training/jb421/HeaderProcessor.java
+++ hybrid-routes/src/main/java/com/redhat/training/jb421/HeaderProcessor.java
@@ -7,26 +7,27 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class HeaderProcessor implements Processor {
-	
+
 	final private static Logger log = LoggerFactory.getLogger(HeaderProcessor.class);
-	
+
 	final private static String JOURNAL_URI = "file:orders?fileExist=Append&fileName=journal-";
-	final private static String QUEUE_URI = "activemq:queue:abc";
-	
-	final private static String XPATH_VENDOR = "/order/orderItems/orderItem/orderItemPublisherName/text()";
+	final private static String QUEUE_URI = "activemq:queue:namming";
+
+	//	final private static String XPATH_VENDOR = "/order/orderItems/orderItem/orderItemPublisher/text()";
+	final private static String XPATH_VENDOR = "/journal/order/orderItems/orderItem/orderItemPublisher/text()";
 
 	@Override
 	public void process(Exchange exchange) throws Exception {
-		log.info("Generating destinationURI header..");
+		log.info("Generating destinationURI header...");
 		String orderXml = exchange.getIn().getBody(String.class);
 		String vendorName = XPathBuilder.xpath(XPATH_VENDOR).evaluate(exchange.getContext(), orderXml);
-		log.info("Vendor: "+vendorName);
-		
-		if("ABC Company".equals(vendorName)){
+		log.info("vendor : " + vendorName);
+
+		if ("Namming".equals(vendorName))
 			exchange.getIn().setHeader("destinationURI", QUEUE_URI);
-		}else{
-			exchange.getIn().setHeader("destinationURI", JOURNAL_URI+vendorName+".txt");
-		}
+		else
+			exchange.getIn().setHeader("destinationURI", JOURNAL_URI + vendorName + ".txt");
+
 	}
 
 }
