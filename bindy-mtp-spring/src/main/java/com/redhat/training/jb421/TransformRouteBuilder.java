6e013b0ff45b3eae9b3b63b1d6dd6a0a0d5ed15d
Ihsan Arif Rahman <ihsan.android@gmail.com>
2018-02-27 11:08:08 +0700

    success build and run spring

--- bindy-mtp-spring/src/main/java/com/redhat/training/jb421/TransformRouteBuilder.java
+++ bindy-mtp-spring/src/main/java/com/redhat/training/jb421/TransformRouteBuilder.java
@@ -3,19 +3,18 @@ package com.redhat.training.jb421;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.dataformat.bindy.csv.BindyCsvDataFormat;
 
-
 import com.redhat.training.jb421.model.CatalogItem;
 
 public class TransformRouteBuilder extends RouteBuilder {
 
-	public static String SRC_FOLDER = "/home/ihsan/jboss/bindy-mtp/items/incoming";
+	public static String SRC_FOLDER = "/home/ihsan/Documents/jboss/bindy-mtp-spring/items/incoming";
 
 	BindyCsvDataFormat bindy = new BindyCsvDataFormat(CatalogItem.class);
 
 	@Override
 	public void configure() throws Exception {
-		from("file:" + SRC_FOLDER).transform(body().regexReplaceAll("\n", "\r\n")).unmarshal(bindy)
-				.wireTap("direct:loggingSystem").to("mock:inventorySystem");
+		from("file:" + SRC_FOLDER).transform(body().regexReplaceAll("\n", "\r\n")).log("${body}").unmarshal(bindy)
+				.log("menuju logging system").wireTap("direct:loggingSystem").to("mock:inventorySystem");
 
 		from("direct:loggingSystem").split().simple("${body}").convertBodyTo(String.class).log("${body}")
 				.to("mock:newItemsFeed");
