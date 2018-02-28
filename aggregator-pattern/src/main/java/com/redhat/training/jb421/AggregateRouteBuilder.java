4b91951130565d072bbcbc5b6585b700e0ae50d0
Ihsan Arif Rahman <ihsan.android@gmail.com>
2018-02-28 10:05:14 +0700

    create project aggregator pattern by blueprint

---
+++ aggregator-pattern/src/main/java/com/redhat/training/jb421/AggregateRouteBuilder.java
@@ -0,0 +1,34 @@
+package com.redhat.training.jb421;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.dataformat.bindy.fixed.BindyFixedLengthDataFormat;
+
+import com.redhat.training.jb421.model.Order;
+
+public class AggregateRouteBuilder extends RouteBuilder {
+
+	public static String SRC_FOLDER = "/home/ihsan/Documents/jboss/aggregator-pattern/orders/incoming";
+	public static String OUTPUT_FOLDER = "/home/ihsan/Documents/jboss/aggregator-pattern/orders/outgoing";
+
+	BindyFixedLengthDataFormat bindy = new BindyFixedLengthDataFormat(Order.class);
+
+	@Override
+	public void configure() throws Exception {
+		from("file://" + SRC_FOLDER)
+		.split()
+		.tokenize("\n")
+		.streaming()
+		.unmarshal(bindy)
+		.aggregate(constant(true), new ArrayListAggregationStrategy())
+		.completionSize(3)
+		.completeAllOnStop()
+		.process(new BatchXMLProcessor())
+		.wireTap("direct:orderLogger")
+		.to("file://" + OUTPUT_FOLDER + "?fileName=output.xml&fileExist=Append", "mock:result");
+
+		from("direct:orderLogger")
+		.split()
+		.tokenizeXML("order")
+		.log("${body}");
+	}
+}
