88bb6c9897d6286a762eb7b163d49ed4c344383b
ihsanarifr <ihsan.android@gmail.com>
2018-03-07 17:40:49 +0700

    update transform data

--- transform-data/src/main/java/com/redhat/training/jb421/TransformRouteBuilder.java
+++ transform-data/src/main/java/com/redhat/training/jb421/TransformRouteBuilder.java
@@ -15,30 +15,30 @@ public class TransformRouteBuilder extends RouteBuilder {
 		from("jpa:com.redhat.training.jb421.model.Order?persistenceUnit=oracle"
 				+ "&consumeDelete=false"
 				+ "&consumer.namedQuery=getUndeliveredOrders"
-				+ "&consumer.delay="+BATCH_TIMEOUT
-				+ "&consumeLockEntity=false").log("${body}")
-			//TODO add wire tap to second route
-			.wireTap("direct:updateOrder");
-			//TODO marshal order to XML with JAXB
-//			.marshal().jaxb()
-			//TODO split the order into individual order items
-//			.split(xpath("order/orderItems/orderItem"))
-			//TODO aggregate the order items based on their catalog item id
-//			.aggregate(xpath("orderItem/catalogItem/id"),new ReservationAggregationStrategy())
-//				.completionInterval(BATCH_TIMEOUT)
-//				.completeAllOnStop()
-			//TODO log the reservation XML to the console
-//			.log("${body}")
-//			.setHeader("CatalogItemId",xpath("/reservation/catalogItemId/text()"))
-			//TODO add file producer
-//			.to("file:"+OUTPUT_FOLDER+"?fileName=${header.CatalogItemId}/"
-//					+ "reservation-${date:now:yyyy-MM-dd_HH-mm-ss}.xml");
+				+ "&maximumResults=1"
+				+ "&consumer.delay=30000"
+				+ "&consumeLockEntity=false")
+		.log("${body}")
+		
+		//TODO add wire tap to second route
+		.wireTap("direct:updateOrder");
+		
+		//TODO marshal order to XML with JAXB
+		
+		//TODO split the order into individual order items
+		
+		//TODO aggregate the order items based on their catalog item id
+		
+		//TODO log the reservation XML to the console
+		
+		//TODO add file producer
+		
+		
 		//TODO add second route to update order in the database
 		from("direct:updateOrder")
-			.log("${body}")
-			.process(new DeliverOrderProcessor())
-			.to("jpa:com.redhat.training.jb421.model.Order?persistenceUnit=oracle");
-			
+		.process(new DeliverOrderProcessor())
+		.to("jpa:com.redhat.training.jb421.model.Order?persistenceUnit=oracle");
+		
 	}
 
 }
