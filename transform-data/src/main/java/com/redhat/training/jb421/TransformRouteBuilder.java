91196e3fd80020f3c6c3713edf9d131bdc26d962
ihsanarifr <ihsan.android@gmail.com>
2018-03-07 16:12:46 +0700

    update class

--- transform-data/src/main/java/com/redhat/training/jb421/TransformRouteBuilder.java
+++ transform-data/src/main/java/com/redhat/training/jb421/TransformRouteBuilder.java
@@ -4,7 +4,7 @@ import org.apache.camel.builder.RouteBuilder;
 
 public class TransformRouteBuilder extends RouteBuilder {
 	
-	public static String OUTPUT_FOLDER = "/home/student/jb421/solutions/transform-data/orders/outgoing";
+	public static String OUTPUT_FOLDER = "/home/ihsan/Documents/jboss-fuse/transform-data/orders/outgoing";
 
 	public static Long BATCH_TIMEOUT = 10000L;
 	
@@ -12,31 +12,32 @@ public class TransformRouteBuilder extends RouteBuilder {
 	@Override
 	public void configure() throws Exception {
 		//TODO add jpa consumer
-		from("jpa:com.redhat.training.jb421.model.Order?persistenceUnit=mysql"
+		from("jpa:com.redhat.training.jb421.model.Order?persistenceUnit=oracle"
 				+ "&consumeDelete=false"
 				+ "&consumer.namedQuery=getUndeliveredOrders"
 				+ "&consumer.delay="+BATCH_TIMEOUT
-				+ "&consumeLockEntity=false")
+				+ "&consumeLockEntity=false").log("${body}")
 			//TODO add wire tap to second route
-			.wireTap("direct:updateOrder")
+			.wireTap("direct:updateOrder");
 			//TODO marshal order to XML with JAXB
-			.marshal().jaxb()
+//			.marshal().jaxb()
 			//TODO split the order into individual order items
-			.split(xpath("order/orderItems/orderItem"))
+//			.split(xpath("order/orderItems/orderItem"))
 			//TODO aggregate the order items based on their catalog item id
-			.aggregate(xpath("orderItem/catalogItem/id"),new ReservationAggregationStrategy())
-				.completionInterval(BATCH_TIMEOUT)
-				.completeAllOnStop()
+//			.aggregate(xpath("orderItem/catalogItem/id"),new ReservationAggregationStrategy())
+//				.completionInterval(BATCH_TIMEOUT)
+//				.completeAllOnStop()
 			//TODO log the reservation XML to the console
-			.log("${body}")
-			.setHeader("CatalogItemId",xpath("/reservation/catalogItemId/text()"))
+//			.log("${body}")
+//			.setHeader("CatalogItemId",xpath("/reservation/catalogItemId/text()"))
 			//TODO add file producer
-			.to("file:"+OUTPUT_FOLDER+"?fileName=${header.CatalogItemId}/"
-					+ "reservation-${date:now:yyyy-MM-dd_HH-mm-ss}.xml");
+//			.to("file:"+OUTPUT_FOLDER+"?fileName=${header.CatalogItemId}/"
+//					+ "reservation-${date:now:yyyy-MM-dd_HH-mm-ss}.xml");
 		//TODO add second route to update order in the database
 		from("direct:updateOrder")
+			.log("${body}")
 			.process(new DeliverOrderProcessor())
-			.to("jpa:com.redhat.training.jb421.model.Order?persistenceUnit=mysql");
+			.to("jpa:com.redhat.training.jb421.model.Order?persistenceUnit=oracle");
 			
 	}
 
