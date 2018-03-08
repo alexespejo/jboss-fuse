3db5dc8d362838b2207c3fb4c56ab1ae539594db
ihsanarifr <ihsan.android@gmail.com>
2018-03-08 13:59:20 +0700

    update test-mockup blueprint

--- test-mock/src/test/java/com/redhat/training/jb421/PredicatesTest.java
+++ test-mock/src/test/java/com/redhat/training/jb421/PredicatesTest.java
@@ -1,22 +1,15 @@
 package com.redhat.training.jb421;
 
-import static com.redhat.training.jb421.predicates.OrderPredicates.*;
-import static com.redhat.training.jb421.predicates.OrderPredicates.validateNumberOfItems;
-import static com.redhat.training.jb421.predicates.OrderPredicates.validateZipCode;
-import static org.apache.camel.builder.ExchangeBuilder.anExchange;
-
 import org.apache.camel.Exchange;
+import org.apache.camel.builder.ExchangeBuilder;
 import org.apache.camel.test.junit4.CamelTestSupport;
-import org.apache.camel.test.spring.CamelSpringTestSupport;
-import org.junit.Assert;
 import org.junit.Test;
-import org.springframework.context.support.AbstractApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.redhat.training.domain.Address;
-import com.redhat.training.domain.Customer;
-import com.redhat.training.domain.Order;
-import com.redhat.training.domain.OrderItem;
+import com.redhat.training.jb421.model.Order;
+import com.redhat.training.jb421.model.Customer;
+import com.redhat.training.jb421.model.Address;
+import com.redhat.training.jb421.model.OrderItem;
+import com.redhat.training.jb421.predicates.OrderPredicates;
 
 public class PredicatesTest extends CamelTestSupport {
 
@@ -24,43 +17,43 @@ public class PredicatesTest extends CamelTestSupport {
 	@Test
 	public void testValidZipCodePredicates() {
 		//Evaluate if the validateZipCode predicate is working using the 25411-2211 postal code
-//		Exchange validExchange = anExchange(context).build();
-//		Order order = createNewOrder("25411-2211");
-//		validExchange.getIn().setBody(order);
-//		assertPredicateMatches(validateZipCode(), validExchange);
-		Assert.fail("Not implemented yet");
+		Exchange validExchange = ExchangeBuilder.anExchange(context).build();
+		Order order = createNewOrder("25411-2211");
+		validExchange.getIn().setBody(order);
+		assertPredicateMatches(OrderPredicates.validateZipCode(), validExchange);
+//		Assert.fail("Not implemented yet");
 	}
 
 	@Test
 	public void testInvalidZipCodePredicates(){
 		//Evaluate if the validateZipCode predicate is working using the X7XXX-XX4X postal code (should not pass)
-//		Exchange invalidExchange = anExchange(context).build();
-//		Order invalidOrder = createNewOrder("X7XXX-XX4X");
-//		invalidExchange.getIn().setBody(invalidOrder);
-//		assertPredicateDoesNotMatch(validateZipCode(), invalidExchange);
-		Assert.fail("Not implemented yet");
+		Exchange invalidExchange = ExchangeBuilder.anExchange(context).build();
+		Order invalidOrder = createNewOrder("X7XXX-XX4X");
+		invalidExchange.getIn().setBody(invalidOrder);
+		assertPredicateDoesNotMatch(OrderPredicates.validateZipCode(), invalidExchange);
+//		Assert.fail("Not implemented yet");
 
 	}
 	
 	@Test
 	public void testValidNumberOfItemsPredicates() {
 		//Evaluate if the validateNumberOfItems predicate is working using an order with three order items (should pass)
-//		Exchange validExchange = anExchange(context).build();
-//		Order order = createNewOrder(new OrderItem(), new OrderItem(), new OrderItem());
-//		validExchange.getIn().setBody(order);
-//		assertPredicateMatches(validateNumberOfItems(), validExchange);
-		Assert.fail("Not implemented yet");
+		Exchange validExchange = ExchangeBuilder.anExchange(context).build();
+		Order order = createNewOrder(new OrderItem(), new OrderItem(), new OrderItem());
+		validExchange.getIn().setBody(order);
+		assertPredicateMatches(OrderPredicates.validateNumberOfItems(), validExchange);
+//		Assert.fail("Not implemented yet");
 
 	}
 
 	@Test
 	public void testInvalidNumberOfItemsPredicate(){
 		// Evaluate if the validateNumberOfItems predicate is working using an order with three order items (should not pass)
-//		Exchange invalidExchange = anExchange(context).build();
-//		Order invalidOrder = createNewOrder(new OrderItem());
-//		invalidExchange.getIn().setBody(invalidOrder);
-//		assertPredicateDoesNotMatch(validateNumberOfItems(), invalidExchange);
-		Assert.fail("Not implemented yet");
+		Exchange invalidExchange = ExchangeBuilder.anExchange(context).build();
+		Order invalidOrder = createNewOrder(new OrderItem());
+		invalidExchange.getIn().setBody(invalidOrder);
+		assertPredicateDoesNotMatch(OrderPredicates.validateNumberOfItems(), invalidExchange);
+//		Assert.fail("Not implemented yet");
 
 
 	}
@@ -79,7 +72,7 @@ public class PredicatesTest extends CamelTestSupport {
 	private Order createNewOrder(OrderItem... items) {
 		Order order = new Order();
 		for (OrderItem orderItem : items) {
-			order.getItems().add(orderItem);
+			order.getOrderItems().add(orderItem);
 		}
 		return order;
 	}
